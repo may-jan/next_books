cce629f543be546bf06d75860bc3df66e6956775
may-jan <jaeeun9862@gmail.com>
2025-08-31 11:14:26 +0900

    Section2 SSR 실습1

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,10 +1,31 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+// 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
+// 사전렌더링 과정에서 1번만 실행(오직 서버측에서만)
+export const getServerSideProps = () => {
+  const data = "hello";
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
+
+  // 컴포넌트가 마운트 된 이후에 실행
+  useEffect(() => {
+    console.log(window);
+  }, []);
+
   return (
     <div className={style.container}>
       <section>
