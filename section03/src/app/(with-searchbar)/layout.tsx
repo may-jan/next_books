ab15f0380f6f85dad259f6ffdac07f465de3e131
may-jan <jaeeun9862@gmail.com>
2025-09-12 21:00:32 +0900

    Section3 리액트 서버 컴포넌트 이해하기

--- section03/src/app/(with-searchbar)/layout.tsx
+++ section03/src/app/(with-searchbar)/layout.tsx
@@ -1,9 +1,11 @@
+import Searchbar from "./searchbar";
+
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div>
-      <div>임시 서치바</div>
+      <Searchbar />
       {children}
     </div>
   );
