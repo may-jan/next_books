7a17a8d45f22f4eb624ef656fc5d68310f39615d
may-jan <jaeeun9862@gmail.com>
2025-09-14 23:22:59 +0900

    Section3 네비게이팅

--- section03/src/components/searchbar.tsx
+++ section03/src/components/searchbar.tsx
@@ -1,18 +1,25 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation"; // 앱라우터 버전의 패키지
 
 export default function Searchbar() {
+  const router = useRouter();
   const [search, setSearch] = useState("");
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
+  // 프로그래매틱한 페이지 이동
+  const onSubmit = () => {
+    router.push(`/search?q=${search}`);
+  };
+
   return (
     <div>
       <input value={search} onChange={onChangeSearch} />
-      <button>검색</button>
+      <button onClick={onSubmit}>검색</button>
     </div>
   );
 }
