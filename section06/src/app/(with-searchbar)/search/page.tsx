7cf11b9f67ed2afe80632ba7d0985178d05e9eff
may-jan <jaeeun9862@gmail.com>
2025-09-21 23:15:14 +0900

    Section6 스트리밍, 페이지 스트리밍 적용하기

--- section06/src/app/(with-searchbar)/search/page.tsx
+++ section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,5 +1,6 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
 
 export default async function Page({
   searchParams,
@@ -8,6 +9,7 @@ export default async function Page({
 }) {
   const { q } = await searchParams;
 
+  await delay(1500); // 스트리밍 실습 - 로딩을 지연시키는 함수
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
     { cache: "force-cache" } // 한 번 검색이 된 페이지라면 빠르게 렌더링 할 수 있도록 캐싱
